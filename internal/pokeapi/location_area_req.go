153897cded2c236a06b7b631539a5ae0cbe54df4
gh4rris <gharris90@hotmail.co.uk>
2024-10-03 18:34:07 +0100

    cache added

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -3,6 +3,7 @@ package pokeapi
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -11,6 +12,14 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 	fullURL := baseURL + endpoint
 	if pageURL != nil {
 		fullURL = *pageURL
+	}
+	if entry, ok := c.cache.Get(fullURL); ok {
+		locationAreasResp := LocationAreasResp{}
+		err := json.Unmarshal(entry, &locationAreasResp)
+		if err != nil {
+			return LocationAreasResp{}, err
+		}
+		return locationAreasResp, nil
 	}
 	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
@@ -24,11 +33,15 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 	if resp.StatusCode > 399 {
 		return LocationAreasResp{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
 	}
-	locationAreasResp := LocationAreasResp{}
-	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(&locationAreasResp)
+	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(data, &locationAreasResp)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+	c.cache.Add(fullURL, data)
 	return locationAreasResp, nil
 }
--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/gh4rris/pokedex/internal/pokecache"
 )
 
 const baseURL = "https://pokeapi.co/api/v2"
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, interval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(interval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
